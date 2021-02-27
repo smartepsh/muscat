7459fbd2510031cd6dd30ad8f664d941381186ca
Kenton <smartepsh@gmail.com>
2021-02-27 22:00:01 +0800

    chore: add dialyxir

--- mix.exs
+++ mix.exs
@@ -25,7 +25,8 @@ defmodule Muscat.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, "~> 0.19", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.0", only: :dev, runtime: false}
     ]
   end
 
