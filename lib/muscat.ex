811a180e40148463fb6fd957c6ba03eb97a22137
Kenton <smartepsh@gmail.com>
2021-03-04 12:18:59 +0800

    feat: delegate apis

--- lib/muscat.ex
+++ lib/muscat.ex
@@ -1,2 +1,6 @@
 defmodule Muscat do
+  defdelegate rref(augmented_matrix, opts), to: Muscat.AugmentedMatrix
+  defdelegate rref(augmented_matrix), to: Muscat.AugmentedMatrix
+  defdelegate new(augmented_matrix), to: Muscat.AugmentedMatrix
+  defdelegate new(coefficient_matrix, constant_column), to: Muscat.AugmentedMatrix
 end
