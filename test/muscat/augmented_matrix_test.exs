4b771722171243916114549293df2b55ccf1b3b9
Kenton <smartepsh@gmail.com>
2021-03-04 19:22:48 +0800

    feat: rref flow

--- test/muscat/augmented_matrix_test.exs
+++ test/muscat/augmented_matrix_test.exs
@@ -46,4 +46,64 @@ defmodule Muscat.AugmentedMatrixTest do
       end
     end
   end
+
+  describe "rref/1" do
+    test "success" do
+      assert [1.0] == AugmentedMatrix.new([[1, 1]]) |> AugmentedMatrix.rref()
+
+      assert [11.0, -8.0, -6.0, -7.0] ==
+               AugmentedMatrix.new([
+                 [2, -2, 2, 6, -16],
+                 [2, -1, 2, 4, -10],
+                 [3, -1, 4, 4, -11],
+                 [1, 1, -1, 3, -12]
+               ])
+               |> AugmentedMatrix.rref()
+
+      assert [4.0, 1.0, -2.0] ==
+               AugmentedMatrix.new([
+                 [1, 2, 3, 0],
+                 [3, 4, 7, 2],
+                 [6, 5, 9, 11]
+               ])
+               |> AugmentedMatrix.rref()
+    end
+
+    test "returns infinite_solutions" do
+      assert {:error, :infinite_solutions} ==
+               AugmentedMatrix.new([
+                 [-2, -2, 2, -2, 2, -2],
+                 [1, -5, 1, -1, -3, -1],
+                 [-1, 2, -5, 5, 6, 2],
+                 [-1, 2, 1, -1, 0, 0]
+               ])
+               |> AugmentedMatrix.rref()
+    end
+
+    test "returns approximate_solution" do
+      assert {:error, :approximate_solution} ==
+               AugmentedMatrix.new([
+                 [4, 3],
+                 [7, 5],
+                 [11, 8]
+               ])
+               |> AugmentedMatrix.rref()
+    end
+
+    test "returns no_solution" do
+      assert {:error, :no_solution} =
+               AugmentedMatrix.new([
+                 [3, 4, 2],
+                 [0, 0, 1]
+               ])
+               |> AugmentedMatrix.rref()
+
+      assert {:error, :no_solution} =
+               AugmentedMatrix.new([
+                 [1, 2, 3, 0],
+                 [3, 4, 7, 2]
+               ])
+               |> AugmentedMatrix.rref()
+    end
+  end
 end
